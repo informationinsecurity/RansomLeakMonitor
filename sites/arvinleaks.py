2cd9320abca670873475c8a6352f9703b29ce6b5
informationinsecurity <informationinsecurity@gmail.com>
2021-08-23 10:07:15 -0600

    Update arvinleaks.py

--- sites/arvinleaks.py
+++ sites/arvinleaks.py
@@ -26,6 +26,7 @@ import allinone as aio
 def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_dir,imgbb_key,imgbb_url):
     imgbb_image_url = ""
     mycursor = mydb.cursor()
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'} 
     page = requests.get(ta_url, timeout=30, proxies=proxies)
     soup = BeautifulSoup(page.content, 'html.parser')
 
