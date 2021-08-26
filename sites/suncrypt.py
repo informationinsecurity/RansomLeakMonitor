eec2751e6245a674eae964c6619d69879b36d059
informationinsecurity <informationinsecurity@gmail.com>
2021-08-26 16:47:53 -0600

    Update suncrypt.py

--- sites/suncrypt.py
+++ sites/suncrypt.py
@@ -25,6 +25,7 @@ import allinone as aio
 #working pulling victims of AvosLocker
 
 def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_dir,imgbb_key,imgbb_url):
+    victim_count = 0 
     imgbb_image_url = ""
     mycursor = mydb.cursor()
 
@@ -47,6 +48,7 @@ def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_d
                 date = timestamp
                 print(date)
                 print(victim_links)
+                victim_count += 1
 
                 dupecheck = "SELECT EXISTS(SELECT * from rw_victims where victim like '" + victim + "')"
                 mycursor.execute(dupecheck)
@@ -103,3 +105,4 @@ def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_d
                         if screenshot_success == True:
                             imgbb_image_url = aio.upload_screenshot(victim_screenshot,imgbb_url,imgbb_key)
                         aio.notifications(imgbb_image_url,victim,victim_links, victim_screenshot,ta,screenshot_success)
+    print(victim_count)
