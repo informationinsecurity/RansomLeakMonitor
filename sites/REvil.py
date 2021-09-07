ed10a96e70e324d7ee292f4ffc810e448c4dae10
informationinsecurity <informationinsecurity@gmail.com>
2021-09-07 22:16:19 +0000

    fixes

--- sites/REvil.py
+++ sites/REvil.py
@@ -25,13 +25,9 @@ import allinone as aio
 #working pulling victims of AvosLocker
 
 def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_dir,imgbb_key,imgbb_url):
+    victim_count = 0
     imgbb_image_url = ""
     mycursor = mydb.cursor()
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-    page = requests.get(ta_url, timeout=30, proxies=proxies, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    div = soup.find_all("div", class_="auction-item-info__title")
-    #working for REvil
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
     page = requests.get(ta_url, timeout=30, proxies=proxies, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -42,7 +38,8 @@ def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_d
     max_page_num = int(last_li.find("a").text)
 
     #print(max_page_num
-    for i in range(max_page_num, 0, -1):
+#   for i in range(max_page_num, 0, -1):
+    for i in range(1,5):
        pagenum = str(i)
        baseurl = ta_url
        baseurl = baseurl + "/?page=" + pagenum
@@ -67,71 +64,61 @@ def scrape(ta_url,ta,proxies,timestamp, mydb,writedb,screenshot,workingdir,tbb_d
                date = timestamp
                print("Date is: ")
                print(date)
-               dupecheck = "SELECT EXISTS(SELECT * from rw_victims where victim like '" + victim + "')"
-               mycursor.execute(dupecheck)
-               duperesult = mycursor.fetchall()
-               duperesult = str(duperesult)
-               if duperesult == "[(1,)]":
-                   print("DUPLICATE ENTRY --SKIPPING NOTIFICATION")
-               else:
-                   print("New Victim Found!")
-                   sql = "INSERT IGNORE INTO rw_victims (id, actor, url, victim, date) VALUES (NULL, %s, %s, %s, %s)"
-                   val = (actor, ta_url, victim, date)
-                   if writedb == True:
-                       mycursor.execute(sql, val)
-                       mydb.commit()
-    
-                   dupecheck = "SELECT EXISTS(SELECT * from rw_victims where victim like '" + victim + "')"
-                   mycursor.execute(dupecheck)
-                   duperesult = mycursor.fetchall()
-                   duperesult = str(duperesult)
-                   if duperesult == "[(1,)]":
-                       print("DUPLICATE ENTRY --SKIPPING NOTIFICATION")
-                   else:
-                       print("New Victim Found!")
-                       sql = "INSERT IGNORE INTO rw_victims (id, actor, url, victim, date) VALUES (NULL, %s, %s, %s, %s)"
-                       val = (ta, ta_url, victim, date)
-                       if writedb == True:
-                           mycursor.execute(sql, val)
-                           mydb.commit()
-    
-                       if screenshot == True:
+            
+           victim_count += 1
+           dupecheck = "SELECT EXISTS(SELECT * from rw_victims where victim like '" + victim + "')"
+           mycursor.execute(dupecheck)
+           duperesult = mycursor.fetchall()
+           duperesult = str(duperesult)
+           if duperesult == "[(1,)]":
+               print("DUPLICATE ENTRY --SKIPPING NOTIFICATION")
+           else:
+               print("New Victim Found!")
+               sql = "INSERT IGNORE INTO rw_victims (id, actor, url, victim, date) VALUES (NULL, %s, %s, %s, %s)"
+               val = (ta, ta_url, victim, date)
+               if writedb == True:
+                   mycursor.execute(sql, val)
+                   mydb.commit()
+
+               if screenshot == True:
+                   time.sleep(3)
+                   victim_screenshot = workingdir + "victims/" + victim + ".png"
+                   print("Victim Screenshot Location:   " + victim_screenshot)
+                   #delete old image if exists
+                   if os.path.exists(victim_screenshot):
+                       os.remove(victim_screenshot)
+                   #print("Grabbing Screenshot of " + victim  + " leak site")
+                   out_img = victim_screenshot
+                   try:
+                       # start a virtual display
+                       xvfb_display = start_xvfb()
+                       with TorBrowserDriver(tbb_dir) as driver:
+                           driver.load_url(victim_links)
                            time.sleep(3)
-                           victim_screenshot = workingdir + "victims/" + victim + ".png"
-                           print("Victim Screenshot Location:   " + victim_screenshot)
-                           #delete old image if exists
-                           if os.path.exists(victim_screenshot):
-                               os.remove(victim_screenshot)
-                           #print("Grabbing Screenshot of " + victim  + " leak site")
-                           out_img = victim_screenshot
-                           try:
-                               # start a virtual display
-                               xvfb_display = start_xvfb()
-                               with TorBrowserDriver(tbb_dir) as driver:
-                                   driver.load_url(victim_links)
-                                   time.sleep(3)
-                                   height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight )")
-                                   driver.set_window_size(900,height+100)
-                                   driver.get_screenshot_as_file(out_img)
-                                   print("Screenshot is saved as %s" % out_img)
-                               stop_xvfb(xvfb_display)
-                               #EDIT LATER TO GET VICTIM SCREENSHOTS INTO DB#####
-                               #sql_insert_blob_query = """ INSERT INTO rw_images (id, timestamp, actor, photo) VALUES (NULL,%s,%s,%s)"""
-                               #ta_screenshot_blob = convertToBinaryData(out_img)
-                               #insert_blob_tuple = (timestamp, actor, ta_screenshot_blob)
-                               #result = mycursor.execute(sql_insert_blob_query, insert_blob_tuple)
-                               #mydb.commit()
-                               #def get_base64_encoded_image(image_path):
-                               #    with open(image_path, "rb") as img_file:
-                               #        return base64.b64encode(img_file.read()).decode('utf-8')
-    
-                                #data = get_base64_encoded_image(ta_screenshot)
-                                #encoded_fig = f"data:image/png;base64,{data}"
-                                #print("Image saved to database")
-                                screenshot_success = True
-                           except:
-                               print("Screenshot Failed")
-                               screenshot_success = False
-                           if screenshot_success == True:
-                               imgbb_image_url = aio.upload_screenshot(victim_screenshot,imgbb_url,imgbb_key)
-                           aio.notifications(imgbb_image_url,victim,victim_links, victim_screenshot,ta,screenshot_success)
+                           height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight )")
+                           driver.set_window_size(900,height+100)
+                           driver.get_screenshot_as_file(out_img)
+                           print("Screenshot is saved as %s" % out_img)
+
+                       stop_xvfb(xvfb_display)
+                       #EDIT LATER TO GET VICTIM SCREENSHOTS INTO DB#####
+                       #sql_insert_blob_query = """ INSERT INTO rw_images (id, timestamp, actor, photo) VALUES (NULL,%s,%s,%s)"""
+                       #ta_screenshot_blob = convertToBinaryData(out_img)
+                       #insert_blob_tuple = (timestamp, actor, ta_screenshot_blob)
+                       #result = mycursor.execute(sql_insert_blob_query, insert_blob_tuple)
+                       #mydb.commit()
+                       #def get_base64_encoded_image(image_path):
+                       #    with open(image_path, "rb") as img_file:
+                       #        return base64.b64encode(img_file.read()).decode('utf-8')
+
+                       #data = get_base64_encoded_image(ta_screenshot)
+                       #encoded_fig = f"data:image/png;base64,{data}"
+                       #print("Image saved to database")
+                       screenshot_success = True
+                   except:
+                       print("Screenshot Failed")
+                       screenshot_success = False
+                   if screenshot_success == True:
+                       imgbb_image_url = aio.upload_screenshot(victim_screenshot,imgbb_url,imgbb_key)
+                   aio.notifications(imgbb_image_url,victim,victim_links, victim_screenshot,ta,screenshot_success)
+    print("Total Victim Count for " + ta + ": " + str(victim_count))
